fcae71aa198ef68f6ad4c04da8646984f71bb29f
Moris David Allan Arias <allandavid184@gmail.com>
2026-06-18 15:38:29 -0500

    fix: add --break-system-packages to pip install in build script

--- TiendaMascotasWeb/build_files.sh
+++ TiendaMascotasWeb/build_files.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 echo "Installing requirements..."
-python3 -m pip install -r requirements.txt
+python3 -m pip install -r requirements.txt --break-system-packages
 echo "Collecting static files..."
 python3 manage.py collectstatic --noinput
